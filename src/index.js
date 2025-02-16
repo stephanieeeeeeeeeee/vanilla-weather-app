5b2a3763e677f953ed1e1edeaf0d19e6784481e7
Steph Stone <54378888+stephanieeeeeeeeeee@users.noreply.github.com>
2025-02-16 16:49:36 -0500

    api fixes and style changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 function updateWeather(response) {
-	let currentTemperatureElement = document.querySelector(
-		"#current-temperature"
-	);
-	let currentTemperature = response.data.temperature.current;
-	let currentCityElement = document.querySelector("#current-city");
+	let temperatureElement = document.querySelector("#temperature");
+	let temperature = response.data.temperature.current;
+	let cityElement = document.querySelector("#city");
+	let descriptionElement = document.querySelector("#description");
+	let humidityElement = document.querySelector("#humidity");
+	let windSpeedElement = document.querySelector("#wind-speed");
+	let timeElement = document.querySelector("#time");
+	let date = new Date(response.data.time * 1000);
+	let iconElement = document.querySelector("#icon");
 
-	currentCityElement.innerHTML = response.data.city;
-	currentTemperatureElement.innerHTML = Math.round(currentTemperature);
+	cityElement.innerHTML = response.data.city;
+	temperatureElement.innerHTML = Math.round(temperature);
+	timeElement.innerHTML = formatDate(date);
+	descriptionElement.innerHTML = response.data.condition.description;
+	humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+	windSpeedElement.innerHTML = `${response.data.wind.speed}mp/h`;
+	iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon" />`;
+}
+
+function formatDate(date) {
+	let minutes = date.getMinutes();
+	let hours = date.getHours();
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[date.getDay()];
+
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+
+	return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
-	// separate of concern
-	let apiKey = `e691ce40ft32d16eb370fo057db0ea64`;
+	let apiKey = "e691ce40ft32d16eb370fo057db0ea64";
 	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
 	axios.get(apiUrl).then(updateWeather);
 }
