32ce47cc8699e17b8eda543c869e22a9d3fa785a
Steph Stone <54378888+stephanieeeeeeeeeee@users.noreply.github.com>
2025-02-16 19:23:07 -0500

    updated js for forecast

--- src/index.js
+++ src/index.js
@@ -52,7 +52,32 @@ function handleSearchSubmit(event) {
 	searchCity(searchInput.value);
 }
 
+function displayForecast() {
+	let days = ["Mon", "Tues", "Wed", "Thur", "Fri"];
+	let forecastHtml = "";
+
+	days.forEach(function (day) {
+		forecastHtml =
+			forecastHtml +
+			`
+			<div class="day">
+				<div class="date">${day}</div>
+				<div class="forecast-icon"></div>
+				<div class="temperatures">
+					<div class="forecast-temperature">
+						<strong>100º</strong>
+					</div>
+					<div class="forecast-temperature">90º</div>
+				</div>
+			</div>
+		`;
+	});
+	let forecastElement = document.querySelector("#forecast");
+	forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("New York");
+displayForecast();
